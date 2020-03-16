de14eb0fda19cb0c30c6b8dae260beab343c3f1f
Rajwinder Kaur Rajwinder Kaur <rrajwind1@isimtl.local>
2020-03-16 16:47:00 -0400

    Home Page video, logo and Inner pages links

---
+++ src/components/PageNotFound/pageNotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class pageNotFound extends Component {
+  render() {
+    return (
+      <div className="NotFound">
+        <h3>Sorry, page not found!</h3>
+      </div>
+       );
+  }
+}
+    
+export default pageNotFound;
